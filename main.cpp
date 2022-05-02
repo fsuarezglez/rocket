ff833496e008a550fa94e5e93a7838296e9dd473
Francisco Suárez González <fsuarez@aerolaser.es>
2022-05-03 00:39:07 +0100

    Introducing parameters in main function. Controlling that parameters are valid

--- main.cpp
+++ main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include "rocket.h"
 
-
-
-int main(void) 
+bool check_number(std::string str)
 {
-    rocket theRocket(10);
+    for (char const &c: str)
+    {
+        if (!isdigit(c))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc < 3)
+        return -1;
+
+    std::string arg1 = std::string(argv[1]);
+    if (arg1 != "-l")
+        return -1;
+
+    std::string arg2 = std::string(argv[2]);
+    if (!check_number(arg2))
+        return -1;
+
+    int levels = std::stoi(arg2);
+
+    rocket theRocket(levels);
 
     while (!theRocket.isRocketTail())
     {
         std::string row = theRocket.getNextRow();
         std::cout << row <<std::endl;
     }
-	std::cout << "Blau Ring Labs rules!" <<std::endl;
+
 	return 0;
 }
