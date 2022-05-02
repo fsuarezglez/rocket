4ed40a79b3d9d4423c4d62f228d998bfe2f53c17
Francisco Suárez González <fsuarez@aerolaser.es>
2022-05-02 23:00:58 +0100

    Adding the new class rocket to the project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
- 
-//*
-//* This is just a template
-//*
+#include "rocket.h"
+
+
 
 int main(void) 
 {
+    rocket theRocket;
 	std::cout << "Blau Ring Labs rules!" <<std::endl;
 	return 0;
 }
